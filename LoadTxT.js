0d9d1b2dae441e09790b31aafa04cf95ebc4f7d5
Erk-G <67289162+Erk-G@users.noreply.github.com>
2020-11-25 19:37:03 -0500

    Add files via upload

---
+++ LoadTxT.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+    $("#Link_Awakening_txt").load("review/Link_Awakening.txt");});
+    $(document).ready(function () {
+    $("#Divinity_2_txt").load("review/Divinity_2.txt");});
+    $(document).ready(function () {
+    $("#Banjo_Kazooie_txt").load("review/Banjo_Kazooie.txt");});
+    $(document).ready(function () {
+    $("#Indivisible_txt").load("review/Indivisible.txt");});
+    $(document).ready(function () {
+    $("#Gato_Roboto_txt").load("review/Gato_Roboto.txt");});
+    $(document).ready(function () {
+    $("#Blaster_Master_Z_2_txt").load("review/Blaster_Master_Z_2.txt");});
+    $(document).ready(function () {
+    $("#Control_txt").load("review/Control.txt");});
+    $(document).ready(function () {
+    $("#ZeroRanger_txt").load("review/ZeroRanger.txt");});
+    $(document).ready(function () {
+    $("#Spyro_txt").load("review/Spyro.txt");});
+    $(document).ready(function () {
+    $("#Catherine_txt").load("review/Catherine.txt");});
+    $(document).ready(function () {
+    $("#Slay_the_spire_txt").load("review/Slay_the_spire.txt");});
+    $(document).ready(function () {
+    $("#MHGU_txt").load("review/MHGU.txt");});
+    $(document).ready(function () {
+    $("#Astral_Chain_txt").load("review/Astral_Chain.txt");});
+    $(document).ready(function () {
+    $("#MegaMan_Zero_2_txt").load("review/MegaMan_Zero_2.txt");});
+    $(document).ready(function () {
+    $("#MegaMan_Zero_3_txt").load("review/MegaMan_Zero_3.txt");});
+    $(document).ready(function () {
+    $("#MegaMan_ZX_txt").load("review/MegaMan_ZX.txt");});
+    $(document).ready(function () {
+    $("#Pathologic_2_txt").load("review/Pathologic_2.txt");});
+    $(document).ready(function () {
+    $("#Sonic_CD_txt").load("review/Sonic_CD.txt");});
+    $(document).ready(function () {
+    $("#Doom_Eternal_txt").load("review/Doom_Eternal.txt");});
+    $(document).ready(function () {
+    $("#MegaMan_ZX_Advent_txt").load("review/MegaMan_ZX_Advent.txt");});
+    $(document).ready(function () {
+    $("#Twin_bee_txt").load("review/Twin_bee.txt");});
+    $(document).ready(function () {
+    $("#RE3R_txt").load("review/RE3R.txt");});
+    $(document).ready(function () {
+    $("#FF7R_txt").load("review/FF7R.txt");});
+    $(document).ready(function () {
+    $("#Sonic_2_txt").load("review/Sonic_2.txt");});
+    $(document).ready(function () {
+    $("#Platinum_ZeroEXP_txt").load("review/Platinum_ZeroEXP.txt");});
+    $(document).ready(function () {
+    $("#Katamari_Reroll_txt").load("review/Katamari_Reroll.txt");});
+    $(document).ready(function () {
+    $("#Trials_of_Mana_1995_txt").load("review/Trials_of_Mana_1995.txt");});
+    $(document).ready(function () {
+    $("#Halo_Reach_txt").load("review/Halo_Reach.txt");});
+    $(document).ready(function () {
+    $("#Nioh_2_txt").load("review/Nioh_2.txt");});
+    $(document).ready(function () {
+    $("#Xenoblade_Future_Connected_txt").load("review/Xenoblade_Future_Connected.txt");});
+    $(document).ready(function () {
+    $("#Rune_Factory_4_txt").load("review/Rune_Factory_4.txt");});
+    $(document).ready(function () {
+    $("#Halo_txt").load("review/Halo.txt");});
+    $(document).ready(function () {
+    $("#Mario_Sunshine_txt").load("review/Mario_Sunshine.txt");});
+    $(document).ready(function () {
+    $("#Devil_May_Cry_txt").load("review/Devil_May_Cry.txt");});
+    $(document).ready(function () {
+    $("#Devil_May_Cry_2_txt").load("review/Devil_May_Cry_2.txt");});
+    $(document).ready(function () {
+    $("#Devil_May_Cry_3_txt").load("review/Devil_May_Cry_3.txt");});
+    $(document).ready(function () {
+    $("#Halo_2_txt").load("review/Halo_2.txt");});
+    $(document).ready(function () {
+    $("#Devil_May_Cry_4_txt").load("review/Devil_May_Cry_4.txt");});
+    $(document).ready(function () {
+    $("#DmC_Devil_May_Cry_txt").load("review/DmC_Devil_May_Cry.txt");});
+    $(document).ready(function () {
+    $("#Bloodstained_CotM2_txt").load("review/Bloodstained_CotM2.txt");});
+    $(document).ready(function () {
+    $("#Pokemon_Mystery_Dungeon_DX_txt").load("review/Pokemon_Mystery_Dungeon_DX.txt");});
+    $(document).ready(function () {
+    $("#Double_Dragon_Neon_txt").load("review/Double_Dragon_Neon.txt");});
+    $(document).ready(function () {
+    $("#Halo_3_txt").load("review/Halo_3.txt");});
+    $(document).ready(function () {
+    $("#Last_of_Us_2_txt").load("review/Last_of_Us_2.txt");});
+    $(document).ready(function () {
+    $("#Devil_May_Cry_5_txt").load("review/Devil_May_Cry_5.txt");});
+    $(document).ready(function () {
+    $("#Ghost_of_Tsushima_txt").load("review/Ghost_of_Tsushima.txt");});
+
+    $(document).ready(function () {
+        $("#Top_txt").load("RGR/Top.txt");});
